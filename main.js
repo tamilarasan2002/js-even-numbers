b254d10b01b50b9c8a108eaff498b811d2db3807
D.Tamilarasan <abidm26791@gmail.com>
2022-12-18 07:02:27 -0800

    js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var a,b=1;
-a=prompt("Enter number of even numbers") ;
-for(i=0;i<a;i++){
-    b=b;
-    document.write(b+"<br>");
-    b=b+2
+var a,c;
+a=parseInt(prompt("length of sequence"))
+c=0;
+for(i=0;c<a;i++){
+    c=c+2
+    document.write(c+"<br>")
 }
